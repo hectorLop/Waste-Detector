0dd5845221ba230165cf083cf44e3aa214950c78
hectorLop <lopez.almazan.hector@gmail.com>
2021-12-14 20:13:11 +0100

    refactor: Adapted the dataset to treat each detection as a different image

--- waste_detector/classifier/dataset.py
+++ waste_detector/classifier/dataset.py
@@ -44,33 +44,34 @@ class WasteDatasetClassification(Dataset):
         self.transforms = transforms
         self.config = config
 
-        cols = [col for col in df.columns if col != 'image_id']
-        self.temp_df = self.df.groupby(['image_id'])[cols].agg(lambda x: list(x)).reset_index()
+        #cols = [col for col in df.columns if col != 'image_id']
+        #self.temp_df = self.df.groupby(['image_id'])[cols].agg(lambda x: list(x)).reset_index()
         
-        self.image_info = collections.defaultdict(dict)
+        #self.image_info = collections.defaultdict(dict)
 
-        for index, row in self.temp_df.iterrows():
-            self.image_info[index] = {
-                'image_id': np.unique(row['image_id'])[0],
-                'height': np.unique(row['height'])[0],
-                'width': np.unique(row['width'])[0],
-                'area': list(row['area']),
-                'iscrowd': list(row['iscrowd']),
-                'image_path': row['filename'][0],
-                'bboxes': list(row['bbox']),
-                'categories': list(row['category_id']),
-            }
+        #for index, row in self.temp_df.iterrows():
+         #   self.image_info[index] = {
+         #       'image_id': np.unique(row['image_id'])[0],
+          #      'height': np.unique(row['height'])[0],
+           #     'width': np.unique(row['width'])[0],
+            #    'area': list(row['area']),
+             #   'iscrowd': list(row['iscrowd']),
+              #  'image_path': row['filename'][0],
+         #       'bboxes': list(row['bbox']),
+          #      'categories': list(row['category_id']),
+           # }
     
     def __getitem__(self, idx):
-        info = self.image_info[idx]
+        info = self.df.iloc[idx, :]
+        #info = self.image_info[idx]
 
         # Read the image and rotate it if neccesary
-        img = read_img(info['image_path'])
-        bbox = torch.as_tensor(info['bbox'][0])
+        img = read_img(info['filename'])
+        bbox = torch.as_tensor(info['bbox'])
         bbox = torchvision.ops.box_convert(bbox, 'xywh', 'xyxy')
         bbox = torchvision.ops.clip_boxes_to_image(bbox, size=(img.shape[:2]))
-        img = crop_img_to_bbox(img, info['bbox'][0])
-        label = info['categories'][0]
+        img = crop_img_to_bbox(img, bbox)
+        label = info['category_id']
         
         if self.transforms:
                 augs = A.Compose(self.transforms)
@@ -86,4 +87,4 @@ class WasteDatasetClassification(Dataset):
         return img, label
 
     def __len__(self):
-        return len(self.image_info)
+        return len(self.df)
